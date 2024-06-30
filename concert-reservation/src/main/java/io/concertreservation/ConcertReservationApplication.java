439ed4f67d85eacf7b49e729724bd1b5f40673cd
renechoi <115696395+renechoi@users.noreply.github.com>
2024-06-30 22:46:24 +0900

    feat: project init

---
+++ concert-reservation/src/main/java/io/concertreservation/ConcertReservationApplication.java
@@ -0,0 +1,13 @@
+package io.concertreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertReservationApplication.class, args);
+	}
+
+}
