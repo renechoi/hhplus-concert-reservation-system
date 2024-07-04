560d0b19986835cba217859e5a13dc4071857fea
renechoi <115696395+renechoi@users.noreply.github.com>
2024-07-04 23:48:28 +0900

    chore: unversioned

---
+++ eureka-service/src/test/java/io/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ gateway-service/src/main/java/io/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package io.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
